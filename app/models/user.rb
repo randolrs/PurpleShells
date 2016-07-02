bd1bb51d0d40f3451ee973378e573dc3238f9d75
randolrs <r.shane.randolph@gmail.com>
2016-07-01 21:57:10 -0500

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+         def full_name
+
+         	if self.name
+
+         		return self.name
+         	else
+
+         		return nil
+
+         	end
+         	
+         end
 end
